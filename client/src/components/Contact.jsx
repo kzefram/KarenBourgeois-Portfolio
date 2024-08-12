f782857c8be21a7aa4c9187f7614fc600652cd7a
kzefram <karenabourgeois@gmail.com>
2024-08-12 16:01:46 -0300

    Checking required for the form on the contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
                 <p className="py-6">I would love to hear from you</p>
             </div>
             
-            <input className='bg-[#ccd6f6] text-[#153243] p-2' type="text" required='true' placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] text-[#153243]' type="email" required='true' placeholder='Email' name='email' />
+            <input required='true' className='bg-[#ccd6f6] text-[#153243] p-2' type="text" placeholder='Name' name='name' />
+            <input required='true' className='my-4 p-2 bg-[#ccd6f6] text-[#153243]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] text-[#153243] p-2' name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-[#B4B8AB] group border-2 px-4 py-3 my-8 mx-auto flex items-center hover:bg-[#F4F9E9] hover:boarder-[#F4F9E9] hover:text-[#284B63]'>Let&apos;s Connect</button>
         </form>
